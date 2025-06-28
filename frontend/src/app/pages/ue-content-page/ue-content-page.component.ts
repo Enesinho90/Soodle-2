f76a441a0cef47822e656a3aef721f826dae29ce
Enesinho90 <enesdu90800@gmail.com>
2025-06-28 17:53:52 +0200

    Partie finis

--- frontend/src/app/pages/ue-content-page/ue-content-page.component.ts
+++ frontend/src/app/pages/ue-content-page/ue-content-page.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { SearchBarComponent } from "../../components/contenue-ue/search-bar/search-bar.component";
-import { PostComponent } from "../../components/contenue-ue/post/post.component";
 import { ListPostComponent } from "../../components/contenue-ue/list-post/list-post.component";
 import { CourseHeaderComponent } from "../../components/header/course-header.component";
 
 @Component({
   selector: 'app-ue-content-page',
-  imports: [RouterModule, SearchBarComponent, PostComponent, ListPostComponent, CourseHeaderComponent],
+  imports: [RouterModule, SearchBarComponent, ListPostComponent, CourseHeaderComponent],
   templateUrl: './ue-content-page.component.html',
   styleUrl: './ue-content-page.component.css'
 })
